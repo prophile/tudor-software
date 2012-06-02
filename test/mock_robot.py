f0687129ec96882edb9cd188c49b6440d2708237
Alastair Lynn <arplynn@gmail.com>
2012-06-03 00:30:25 +0100

    Made the dummy mock robot print to the background
    Previously, the mock robot filled stdout with many many things,
    masking the rest of the output. It now writes to a file in /tmp
    (/tmp/status) which can be observed with tail -f.

--- test/mock_robot.py
+++ test/mock_robot.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import multiprocessing, time
 
 BatteryLevel = namedtuple('BatteryLevel', ('voltage', 'current'))
 
@@ -11,35 +12,59 @@ class MockPower(object):
         pass
 
 class MockMotor(object):
-    def __init__(self, n):
+    def __init__(self, n, callback):
         self.id = n
+        self.callback = callback
         self.duty = 0
 
     def _get_target(self):
         return self.duty
 
     def _set_target(self, value):
+        self.callback(self.id, value)
         self.duty = value
-        print 'Motor {0} duty set to {1}'.format(self.id, self.duty)
 
     target = property(fget = _get_target, fset = _set_target)
 
 class MockServo(object):
-    def __init__(self, n):
+    def __init__(self, n, callback):
         self.id = n
+        self.callback = callback
         self.servo_values = [50] * 8
 
     def __getitem__(self, n):
         return self.servo_values[n]
 
     def __setitem__(self, n, value):
-        print 'Servo {0}:{1} set to {2}'.format(self.id, n, value)
+        self.callback(self.id, n, value)
         self.servo_values[n] = value
 
 class MockRobot(object):
     def __init__(self, nmotors = 3, nservos = 1):
-        self.motors = [MockMotor(n) for n in xrange(nmotors)]
-        self.servos = [MockServo(n) for n in xrange(nservos)]
+        def print_info(pipe):
+            data_dictionary = {}
+            for n in xrange(nmotors):
+                data_dictionary["M{0}".format(n)] = 0.0
+            for n in xrange(nservos):
+                for m in xrange(8):
+                    data_dictionary["S{0}:{1}".format(n, m)] = 0.0
+            while True:
+                time.sleep(0.2)
+                while pipe.poll():
+                    key, value = pipe.recv()
+                    data_dictionary[key] = value
+                data_output = ' | '.join('{0}: {1: 4}'.format(x, int(y)) for (x, y) in sorted(data_dictionary.items()))
+                with open('/tmp/status', 'w') as f:
+                    f.write('{0}\n'.format(data_output))
+        parent_pipe, child_pipe = multiprocessing.Pipe()
+        self._info_thread = multiprocessing.Process(target = print_info, args = (child_pipe,))
+        def motor_callback(n, value):
+            parent_pipe.send(('M{0}'.format(n), value))
+        def servo_callback(n, m, value):
+            parent_pipe.send(('S{0}:{1}'.format(n, m), value))
+        self.motors = [MockMotor(n, motor_callback) for n in xrange(nmotors)]
+        self.servos = [MockServo(n, servo_callback) for n in xrange(nservos)]
         self.power = MockPower()
         self.usbkey = '.'
+        self._info_thread.start()
 
