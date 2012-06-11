746a257fafa7a46c2f9a12cda095903a1d5cae2f
Alastair Lynn <arplynn@gmail.com>
2012-06-11 20:38:32 +0100

    Added useful status outputs in the controller

--- controller/controller.py
+++ controller/controller.py
@@ -19,6 +19,7 @@ class _ConfigurationLoader(object):
     def load_wheels(self):
         config = self.configuration['wheels']
         for n, wheel in config.iteritems():
+            print "\tconfiguring wheel {0}...".format(n)
             self.controller.wheels.add_wheel(n,
                                              wheel['distance'],
                                              math.radians(wheel['angle']),
@@ -26,38 +27,52 @@ class _ConfigurationLoader(object):
                                              wheel.get('calibration', 0.01))
     def load_grabber(self):
         config = self.configuration['grabber']
+        print "\tconfiguring vertical grabber servo..."
         self.controller.grabber.vertical_servo = config['servos']['vertical']
+        print "\tconfiguring left grabber servo..."
         self.controller.grabber.left_servo = config['servos']['left']
+        print "\tconfiguring right grabber servo..."
         self.controller.grabber.right_servo = config['servos']['right']
 
     def load_camera_mount(self):
         config = self.configuration['camera_mount']
         if 'yaw' in config:
+            print "\tconfiguring camera mount yaw servo..."
             self.controller.camera_mount.yaw_servo = tuple(config['yaw']['servo'])
             self.controller.camera_mount.yaw_calibration = tuple(config['yaw']['calibration'])
         if 'pitch' in config:
+            print "\tconfiguring camera mount pitch servo..."
             self.controller.camera_mount.pitch_servo = tuple(config['pitch']['servo'])
             self.controller.camera_mount.pitch_calibration = tuple(config['pitch']['calibration'])
         if 'headlights' in config:
+            print "\tconfiguring camera mount headlight GPIO servo..."
             self.controller.camera_mount.headlights_servo = tuple(config['headlights']['servo'])
 
     def load_control(self):
-        self.controller.control_settings = self.configuration.get('control', {})
+        print "\tconfiguring control settings..."
+        if 'control' in self.configuration:
+            self.controller.control_settings = self.configuration['control']
+        else:
+            print "\t\tno control configuration found"
 
 class Controller(object):
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, robot, calibration_file = None):
         self.robot = robot
+        self.name = DEFAULT_NAME
+        self.control_settings = None
+        print "Creating mech interfaces..."
         self.wheels = wheels.WheelSystem(robot)
         self.grabber = grabber.Grabber(robot)
         self.camera_mount = camera_mount.CameraMount(robot)
+        print "Loading calibration file..."
         self._load_calibration(calibration_file)
+        print "Setting up main loop..."
         self.exit = False
         self.update_frequency = 30.0
-        self.name = DEFAULT_NAME
         self.scheduler = None
-        self.control_settings = None
+        print "Ready; my name is {0}".format(self.name)
 
     def tick(self):
         pass
@@ -69,10 +84,12 @@ class Controller(object):
         pass
 
     def run_once(self):
+        print ".",
         self.tick()
         self.wheels.ramp_update()
 
     def run(self):
+        print "Creating scheduler..."
         self.scheduler = sched.scheduler(time.time, time.sleep)
         self.exit = False
         self.start()
@@ -80,7 +97,9 @@ class Controller(object):
             self.run_once()
             if not self.exit:
                 self.scheduler.enter(1.0 / self.update_frequency, -1, cycle, ())
+        print "Prerolling..."
         cycle()
+        print "Entering main loop..."
         self.scheduler.run()
         self.stop()
 
